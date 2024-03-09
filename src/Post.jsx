fda89011ae2e98c313f73e46465c869c18e6275c
SohelDewan <sohelds241@gmail.com>
2024-03-09 22:05:52 +0600

    first commit

---
+++ src/Post.jsx
@@ -0,0 +1,13 @@
+import './Post.css'
+export default function Post({post}){
+    const {title, body, id, userId} = post;
+    return (
+        <div className='post'>
+            <p><small>userId: {userId}</small></p>
+            <p><small>postId: {id}</small></p>
+            <h5>Title: {title}</h5>
+            <p>{body}</p>
+
+        </div>
+    )
+}
